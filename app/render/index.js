8f03dc54b4520400363b093d706e05e3468b4341
meslzy <i@meslzy.com>
2021-02-14 11:29:53 +0300

    first commit

---
+++ app/render/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "./source/app";
+import "./index.scss";
+
+window.dev = process.env.NODE_ENV === "development";
+
+ReactDOM.render(<App/>, document.getElementById("app"));
